8c9121ebc4ca85d46343f5cc36a317136820a9da
graffsmile <sevulybin@yandex.ru>
2025-02-07 12:46:20 +0500

    Update config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+# Загрузка переменных окружения из файла .env
+dotenv_path = 'config.env'
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+GROUP_TOKEN = os.getenv('GROUP_TOKEN')
+USER_TOKEN = os.getenv('USER_TOKEN')
+GROUP_ID = os.getenv('GROUP_ID')
+login = os.getenv('LOGIN')
+password_psq = os.getenv('PASSWORD_PSQ')
+name_db = os.getenv('NAME_DB')
